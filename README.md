6eda74fa8b26b7b073816040386547ec23d4b1f1
sonalimeh <sonalimeh1912@gmail.com>
2021-05-21 19:31:36 +0530

    Added all APIs

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 2) Go the airbus_backend folder and type "pip install pipenv"
 3) Go the airbus_backend folder and type "pipenv shell"
 4) Go the airbus_backend folder and type "pipenv install"
-5) Go the airbus_backend folder/src and type "flask run"
+5) Go the airbus_backend folder and type "python -m spacy download en_core_web_sm"
+6) Go the airbus_backend folder/src and type "flask run"
+
--- app.py
+++ app.py
@@ -1,26 +1,37 @@
+import chatterbot
 from flask import Response, jsonify, Flask, request
 from chatterbot import ChatBot
 from pymongo import MongoClient
 from flask import jsonify
+import json
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from bson import ObjectId
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
 mongodb_uri = 'mongodb+srv://User_1:Qwerty@cluster0.q5slf.mongodb.net/bktlist?retryWrites=true&w=majority'
 
+print(chatterbot.__file__) 
 client = MongoClient(mongodb_uri)
 bugs = client.bktlist.Bugs
 feedback = client.bktlist.feedback
+announcement = client.bktlist.announcement
 
 english_bot = ChatBot("English Bot", 
                      storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
                      database = 'bktlist',
                      database_uri = mongodb_uri)
 trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
-
+# trainer.train("chatterbot.corpus.english")
+trainer.load()
 
 app = Flask(__name__)
 
-app.config["DEBUG"] = True
-app.config["TESTING"] = False
 app.config['SECRET_KEY']  = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route('/health-check', methods=['GET'])
@@ -30,8 +41,7 @@ def health():
 @app.route('/chatbot', methods=['GET'])
 def chatbot():
     userText = request.get_json()
-    print(userText)
-    return str(english_bot.get_response(userText['msg']))
+    return jsonify(reply=str(english_bot.get_response(userText['msg'])))
 
 @app.route('/bugs', methods=['GET', 'POST'])
 def bugs_():
@@ -42,7 +52,7 @@ def bugs_():
         return "Bug sucessfully registered"
     if request.method == 'GET':
         bug = bugs
-        return str(list(bug.find({})))
+        return json.dumps(list(bug.find({})), cls=JSONEncoder)
 
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback_():
@@ -53,4 +63,15 @@ def feedback_():
         return "Feedback sucessfully registered"
     if request.method == 'GET':
         feedbacks = feedback
-        return str(list(feedbacks.find({})))
+        return json.dumps(list(feedbacks.find({})), cls=JSONEncoder)
+
+@app.route('/announcement', methods=['GET', 'POST'])
+def announcement_():
+    if request.method == 'POST':
+        userText = request.get_json()
+        announce = announcement
+        announce.insert(userText)
+        return "Feedback sucessfully registered"
+    if request.method == 'GET':
+        announce = announcement
+        return json.dumps(list(announce.find({})), cls=JSONEncoder)
